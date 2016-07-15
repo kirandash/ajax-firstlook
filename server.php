3826a283fa058daad60a8d1319c45f17207e41b7
kirandash <thekirankumardash@gmail.com>
2016-07-15 21:07:10 +0530

    POST Method for AJAX using Javascript

--- server.php
+++ server.php
@@ -1,7 +1,7 @@
 <?php
 	
-	$data = $_GET;
-	$user_text = $_GET['user_text'];
+	$data = $_REQUEST; //_REQUEST will accept either $_GET or $_POST
+	$user_text = $_REQUEST['user_text'];
 	$response = strtoupper( $user_text );
 	echo $response;
 	
